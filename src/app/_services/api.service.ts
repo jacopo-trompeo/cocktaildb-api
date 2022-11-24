f4a44334571f81fd6bc3437975bcb11831334381
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-24 17:22:42 +0100

    Implemented all home page api calls

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -7,24 +7,52 @@ import { DrinkApiResType, DrinkType } from '../_models/drink.model';
   providedIn: 'root',
 })
 export class ApiService {
-  BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
+  private BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
 
   constructor(private http: HttpClient) {}
+
+  private compareDrinkNames(a: DrinkType, b: DrinkType) {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+      return -1;
+    }
+
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1;
+    }
+
+    return 0;
+  }
 
   getDrinksByFirstLetter(firstLetter: string) {
     return this.http
       .get<DrinkApiResType>(`${this.BASE_URL}/search.php?f=${firstLetter}`)
       .pipe(
         map((res) => {
-          return res.drinks.map((drink) => {
-            return {
-              id: drink.idDrink,
-              name: drink.strDrink,
-              category: drink.strCategory,
-              image: drink.strDrinkThumb,
-            } as DrinkType;
-          });
+          return res.drinks
+            .map((drink) => {
+              return {
+                id: drink.idDrink,
+                name: drink.strDrink,
+                category: drink.strCategory,
+                image: drink.strDrinkThumb,
+              } as DrinkType;
+            })
+            .sort(this.compareDrinkNames);
         })
       );
+  }
+
+  getRandomDrink() {
+    return this.http.get<DrinkApiResType>(`${this.BASE_URL}/random.php`).pipe(
+      map((res) => {
+        const drink = res.drinks[0];
+        return {
+          id: drink.idDrink,
+          name: drink.strDrink,
+          category: drink.strCategory,
+          image: drink.strDrinkThumb,
+        } as DrinkType;
+      })
+    );
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../_services/api.service';
+import { DrinkType } from 'src/app/_models/drink.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  firstLetter!: string;
+  paginationLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  drinks: DrinkType[] = [];
+  featuredDrink: DrinkType = {} as DrinkType;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getDrinksByFirstLetter('a').subscribe((res) => {
-      console.log(res);
+    this.firstLetter = localStorage.getItem('firstLetter') || 'A';
+
+    this.getFeaturedDrink();
+    this.getDrinksByFirstLetter();
+  }
+
+  getDrinksByFirstLetter(firstLetter: string = this.firstLetter) {
+    this.firstLetter = firstLetter;
+    localStorage.setItem('firstLetter', this.firstLetter);
+
+    this.apiService
+      .getDrinksByFirstLetter(this.firstLetter)
+      .subscribe((drinks) => {
+        this.drinks = drinks;
+        console.table(this.drinks);
+      });
+  }
+
+  getFeaturedDrink() {
+    this.apiService.getRandomDrink().subscribe((drink) => {
+      this.featuredDrink = drink;
+      console.table(this.featuredDrink);
     });
   }
 }
